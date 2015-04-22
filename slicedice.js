83ea52c2210db03ebea6eaf69ac1f5e1cf4fd5aa
scardine <paulos@xtend.com.br>
2015-04-22 11:58:53 -0300

    working...

--- slicedice.js
+++ slicedice.js
@@ -6,7 +6,7 @@ var SliceDice = function(config) {
         normalize: parseFloat,
         slices: 5
     };
-    for (var attr in config) { this.config[attr] = config[attr];}
+    _.assign(this.config, config);
 
     this.config.sample = _.map(this.config.sample, this.normalize).sort(function (x, y) {return x - y;});
     this.unique = _.unique(this.config.sample);
@@ -14,16 +14,54 @@ var SliceDice = function(config) {
     this.max = d3.max(this.config.sample);
     this.min = d3.min(this.config.sample);
 
+    var getScale = function(name) {
+        var config = self.config;
+
+        switch (name) {
+            case 'quantile': {
+                return d3.scale.quantile().range(_.range(config.slices)).domain(config.sample);
+            }
+            case 'linear': {
+                return function(v) {
+                    var slice_size = Math.round((config.max - config.min) / config.slices);
+                    return Math.floor(v / slice_size);
+                }
+            }
+            case 'custom': {
+                return function(v) {
+                    if (v < self.min) return 0;
+                    if (v >= self.max) return self.config.slices - 1;
+                    if (self.config.ranges === undefined) throw 'For custom scales you must provide a "ranges" parameter.';
+                    for (var i = 0; i < config.slices; i++) {
+                        if (v >= self.config.ranges[i].start && v < self.config.ranges[i].end) return i;
+                    }
+                }
+            }
+            case 'log': {
+                return d3.scale.log().range(_.range(config.slices)).domain(config.sample);
+            }
+        }
+
+        throw "Unknown scale";
+    };
+
     if (this.unique.length <= this.config.slices) {
         this.config.slices = this.unique.length;
         this.ranges = _.map(this.unique, function(v, i) {
             return {index: i, start: v, end: v};
         });
     } else {
-        var scale = this.config.scale;
-        if (scale === undefined) {
-            scale = d3.scale.quantile().range(_.range(this.config.slices)).domain(this.config.sample);
+        var scale;
+        if (this.config.scale === undefined) {
+            scale = getScale('quantile');
+        } else if (typeof this.config.scale == 'string') {
+            scale = getScale(scale);
+        } else if (typeof this.config.scale == 'function') {
+            scale = this.config.scale;
+        } else {
+            throw "invalid scale";
         }
+
         this.ranges = _.map(_.range(this.config.slices), function(v, i) {
             return {index: i, start: self.min, end: self.min};
         });
@@ -38,10 +76,58 @@ var SliceDice = function(config) {
     }
 };
 
+
+var getScale = function(name) {
+    var config = this.config;
+
+    switch (name) {
+        case 'quantile': {
+            return d3.scale.quantile().range(_.range(config.slices)).domain(config.sample);
+        }
+        case 'linear': {
+            return function(v) {
+                var slice_size = Math.round((config.max - config.min) / config.slices);
+                return Math.floor(v / slice_size);
+            }
+        }
+        case 'custom': {
+            return function(v) {
+                if (v < self.min) return 0;
+                if (v >= self.max) return self.config.slices - 1;
+                if (self.config.ranges === undefined) throw 'For custom scales you must provide a "ranges" parameter.';
+                for (var i = 0; i < config.slices; i++) {
+                    if (v >= self.config.ranges[i].start && v < self.config.ranges[i].end) return i;
+                }
+            }
+        }
+        case 'log': {
+            return d3.scale.log().range(_.range(config.slices)).domain(config.sample);
+        }
+    }
+
+    throw "Unknown scale";
+};
+
+
 SliceDice.prototype.getRange = function(v) {
     for(var i=0; i < this.ranges.length; i++) {
         if(v >= this.ranges[i].start && v < this.ranges[i].end) return this.ranges[i];
     }
     if (v < this.min) return this.ranges[0];
     return this.ranges[this.ranges.length-1];
+};
+
+
+SliceDice.prototype.getColors = function(type) {
+    switch (type) {
+        case 'degrade': {
+            var range = _.range(this.config.slices);
+            var start = arguments[1] || '#009900';
+            var end = arguments[2] || '#003300';
+            return _.map(range, d3.scale.linear().range([start, end]).domain([0, range.length - 1]));
+        }
+        default: {   // 'palette'
+            return _.map(_.range(this.config.slices), d3.scale[arguments[1] || 'category20']())
+        }
+    }
 };
